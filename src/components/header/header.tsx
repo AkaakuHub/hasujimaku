ae226ecccaedbb19b2ec1628416ee216a8b60d0b
AkaakuHub <akaaku2021media01@gmail.com>
2026-07-16 15:50:04 +0900

    chore: SEO対策

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -11,15 +11,47 @@ type Props = {
 const Header: React.FC<Props> = ({ themeColor, changeThemeColor }) => {
   return (
     <Box component="header">
-      <AppBar position="static" sx={{ bgcolor: themeColor, color: "common.black" }}>
-        <Toolbar>
-          <Button
-            color="inherit"
-            onClick={changeThemeColor}
-            sx={{ justifyContent: "center", flexGrow: 1, fontSize: "1.25rem", fontWeight: 400 }}
+      <AppBar
+        position="static"
+        sx={{
+          bgcolor: themeColor,
+          color: "common.black",
+        }}
+      >
+        <Toolbar
+          disableGutters
+          sx={{
+            m: 0,
+            p: 0,
+            alignItems: "stretch",
+          }}
+        >
+          <Box
+            component="h1"
+            sx={{
+              m: 0,
+              p: 0,
+              width: "100%",
+              display: "flex",
+              lineHeight: 0,
+            }}
           >
-            活動記録字幕ジェネレーター
-          </Button>
+            <Button
+              color="inherit"
+              onClick={changeThemeColor}
+              title="テーマカラーを変更"
+              sx={{
+                width: "100%",
+                m: 0,
+                borderRadius: 0,
+                justifyContent: "center",
+                fontSize: "1.25rem",
+                fontWeight: 400,
+              }}
+            >
+              蓮ノ空 活動記録字幕ジェネレーター
+            </Button>
+          </Box>
         </Toolbar>
       </AppBar>
     </Box>
